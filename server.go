b43e442df7fdda5d1b49eb157b680db423539fba
William Koller <williamkoller30@gmail.com>
2023-09-27 16:01:34 -0300

    chore: update readme

--- server.go
+++ server.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("<h1>Hello FullCycle v8</h1>"))
+	w.Write([]byte("<h1>Hello FullCycle v9.3</h1>"))
 }
